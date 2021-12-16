49c374b13e2013df19174e2da3fa31ef96bd77af
Spoertm <spoertm@gmail.com>
2021-12-16 22:48:42 +0100

    Minor modifications

--- CoreHtmlToImage.Console/Program.cs
+++ CoreHtmlToImage.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace CoreHtmlToImage.Console
 {
@@ -8,12 +9,14 @@ namespace CoreHtmlToImage.Console
 		{
 			// From HTML string
 			HtmlConverter converter = new();
-			string html = "<div><strong>Hello</strong> World!</div>";
+			const string html = "<div><strong>Hello</strong> World!</div>";
 			byte[] htmlBytes = converter.FromHtmlString(html);
 
 			// From URL
 			byte[] urlBytes = converter.FromUrl("http://google.com", 800, format: ImageFormat.Png, quality: 90);
-			File.WriteAllBytes("D:\\image.png", urlBytes);
+			string path = Path.Combine(AppContext.BaseDirectory, "image.png");
+			File.WriteAllBytes(path, urlBytes);
+			System.Console.WriteLine($"Saved image to {path}");
 		}
 	}
 }
--- CoreHtmlToImage/HtmlConverter.cs
+++ CoreHtmlToImage/HtmlConverter.cs
@@ -2,45 +2,38 @@
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using System.Runtime.InteropServices;
 
 namespace CoreHtmlToImage
 {
-    /// <summary>
-    /// Html Converter. Converts HTML string and URLs to image bytes
-    /// </summary>
-    public class HtmlConverter
+	/// <summary>
+	/// Html Converter. Converts HTML string and URLs to image bytes
+	/// </summary>
+	public class HtmlConverter
 	{
-		private static string toolFilename = "wkhtmltoimage";
-		private static string directory;
-		private static string toolFilepath;
+		private const string ToolFilename = "wkhtmltoimage";
+		private static readonly string Directory = AppContext.BaseDirectory;
+		private static readonly string ToolFilepath;
 
 		static HtmlConverter()
 		{
-			directory = AppContext.BaseDirectory;
-
-			//Check on what platform we are
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			// Check on what platform we are
+			if (Environment.OSVersion.Platform == PlatformID.Win32NT)
 			{
-				toolFilepath = Path.Combine(directory, toolFilename + ".exe");
+				ToolFilepath = Path.Combine(Directory, ToolFilename + ".exe");
+				if (File.Exists(ToolFilepath))
+					return;
 
-				if (!File.Exists(toolFilepath))
-				{
-					Assembly assembly = typeof(HtmlConverter).GetTypeInfo().Assembly;
-					Type type = typeof(HtmlConverter);
-					string? ns = type.Namespace;
+				Assembly assembly = typeof(HtmlConverter).Assembly;
+				string nameSpace = typeof(HtmlConverter).Namespace!;
 
-					using (Stream? resourceStream = assembly.GetManifestResourceStream($"{ns}.{toolFilename}.exe"))
-					using (FileStream fileStream = File.OpenWrite(toolFilepath))
-					{
-						resourceStream.CopyTo(fileStream);
-					}
-				}
+				using Stream resourceStream = assembly.GetManifestResourceStream($"{nameSpace}.{ToolFilename}.exe")!;
+				using FileStream fileStream = File.OpenWrite(ToolFilepath);
+				resourceStream.CopyTo(fileStream);
 			}
-			else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			else if (Environment.OSVersion.Platform == PlatformID.Unix)
 			{
-				//Check if wkhtmltoimage package is installed on this distro in using which command
-				Process process = Process.Start(new ProcessStartInfo()
+				// Check if wkhtmltoimage package is installed on this distro in using which command
+				Process process = Process.Start(new ProcessStartInfo
 				{
 					CreateNoWindow = true,
 					UseShellExecute = false,
@@ -49,56 +42,52 @@ namespace CoreHtmlToImage
 					RedirectStandardError = true,
 					FileName = "/bin/bash",
 					Arguments = "which wkhtmltoimage",
-				});
+				})!;
 
 				string answer = process.StandardOutput.ReadToEnd();
 				process.WaitForExit();
 
 				if (!string.IsNullOrEmpty(answer) && answer.Contains("wkhtmltoimage"))
-				{
-					toolFilepath = "wkhtmltoimage";
-				}
+					ToolFilepath = "wkhtmltoimage";
 				else
-				{
-					throw new("wkhtmltoimage does not appear to be installed on this linux system according to which command; go to https://wkhtmltopdf.org/downloads.html");
-				}
+					throw new("wkhtmltoimage does not appear to be installed on this linux system according to 'which' command; go to https://wkhtmltopdf.org/downloads.html");
 			}
 			else
 			{
-				//OSX not implemented
-				throw new("OSX Platform not implemented yet");
+				// OSX not implemented
+				throw new("OSX Platform not supported yet");
 			}
 		}
 
-        /// <summary>
-        /// Converts HTML string to image
-        /// </summary>
-        /// <param name="html">HTML string</param>
-        /// <param name="width">Output document width</param>
-        /// <param name="format">Output image format</param>
-        /// <param name="quality">Output image quality 1-100</param>
-        /// <returns></returns>
-        public byte[] FromHtmlString(string html, int width = 1024, ImageFormat format = ImageFormat.Jpg, int quality = 100)
+		/// <summary>
+		/// Converts HTML string to image
+		/// </summary>
+		/// <param name="html">HTML string</param>
+		/// <param name="width">Output document width</param>
+		/// <param name="format">Output image format</param>
+		/// <param name="quality">Output image quality 1-100</param>
+		/// <returns></returns>
+		public byte[] FromHtmlString(string html, int width = 1024, ImageFormat format = ImageFormat.Jpg, int quality = 100)
 		{
-			string filename = Path.Combine(directory, $"{Guid.NewGuid()}.html");
+			string filename = Path.Combine(Directory, $"{Guid.NewGuid()}.html");
 			File.WriteAllText(filename, html);
 			byte[] bytes = FromUrl(filename, width, format, quality);
 			File.Delete(filename);
 			return bytes;
 		}
 
-        /// <summary>
-        /// Converts HTML page to image
-        /// </summary>
-        /// <param name="url">Valid http(s):// URL</param>
-        /// <param name="width">Output document width</param>
-        /// <param name="format">Output image format</param>
-        /// <param name="quality">Output image quality 1-100</param>
-        /// <returns></returns>
-        public byte[] FromUrl(string url, int width = 1024, ImageFormat format = ImageFormat.Jpg, int quality = 100)
+		/// <summary>
+		/// Converts HTML page to image
+		/// </summary>
+		/// <param name="url">Valid http(s):// URL</param>
+		/// <param name="width">Output document width</param>
+		/// <param name="format">Output image format</param>
+		/// <param name="quality">Output image quality 1-100</param>
+		/// <returns></returns>
+		public byte[] FromUrl(string url, int width = 1024, ImageFormat format = ImageFormat.Jpg, int quality = 100)
 		{
 			string imageFormat = format.ToString().ToLower();
-			string filename = Path.Combine(directory, $"{Guid.NewGuid().ToString()}.{imageFormat}");
+			string filename = Path.Combine(Directory, $"{Guid.NewGuid().ToString()}.{imageFormat}");
 
 			string args;
 
@@ -111,39 +100,30 @@ namespace CoreHtmlToImage
 				args = $"--quality {quality} --width {width} -f {imageFormat} {url} \"{filename}\"";
 			}
 
-			Process process = Process.Start(new ProcessStartInfo(toolFilepath, args)
+			Process process = Process.Start(new ProcessStartInfo(ToolFilepath, args)
 			{
 				WindowStyle = ProcessWindowStyle.Hidden,
 				CreateNoWindow = true,
 				UseShellExecute = false,
-				WorkingDirectory = directory,
+				WorkingDirectory = Directory,
 				RedirectStandardError = true,
-			});
+			})!;
 
 			process.ErrorDataReceived += Process_ErrorDataReceived;
 			process.WaitForExit();
 
-			if (File.Exists(filename))
-			{
-				byte[] bytes = File.ReadAllBytes(filename);
-				File.Delete(filename);
-				return bytes;
-			}
+			if (!File.Exists(filename))
+				throw new("Something went wrong. Please check input parameters");
 
-			throw new("Something went wrong. Please check input parameters");
+			byte[] bytes = File.ReadAllBytes(filename);
+			File.Delete(filename);
+			return bytes;
 		}
 
 		private static bool IsLocalPath(string path)
-		{
-			if (path.StartsWith("http"))
-			{
-				return false;
-			}
+			=> !path.StartsWith("http") && new Uri(path).IsFile;
 
-			return new Uri(path).IsFile;
-		}
-
-		private void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+		private static void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
 		{
 			throw new(e.Data);
 		}
